f9db56a3b0c95e4aa32131b4959e0a47ae816c3a
dzmitrok <dzmitrok@hotmail.com>
2015-09-27 12:50:39 +0300

    updated this branch

--- jboss7_standalone-0.1.0/recipes/cumulogic.rb
+++ jboss7_standalone-0.1.0/recipes/cumulogic.rb
@@ -4,7 +4,8 @@ jboss = node['jboss7']
 #download cumulogic test testweb.zip
 
 remote_file "#{Chef::Config[:file_cache_path]}/#{jboss['testweb_archive']}" do
- source "#{jboss['testweb_url']}/#{jboss['testweb_archive']}"
+ #source "#{jboss['testweb_url']}/#{jboss['testweb_archive']}"
+ source node.chef_environment.default_attributes {"application repo"}
  action :create_if_missing
  notifies :run, 'bash[Extract testweb]', :immediately
 end
@@ -14,8 +15,10 @@ end
 bash 'Extract testweb' do
  cwd Chef::Config[:file_cache_path]
  code <<-EOF
- unzip -u #{Chef::Config[:file_cache_path]}/#{jboss['testweb_archive']}
- cp "#{Chef::Config[:file_cache_path]}/testweb/*.war" "#{jboss['jboss_home']}/standalone/deployments/"
+ unzip -u #{Chef::Config[:file_cache_path]}/#{jboss['testweb_archive']} # -d #{Chef::Config[:file_cache_path]}
+ cp "#{Chef::Config[:file_cache_path]}/testweb/testweb.war" "#{jboss['jboss_home']}/standalone/deployments/"
  EOF
  action :run
 end
+
+#load data bag
